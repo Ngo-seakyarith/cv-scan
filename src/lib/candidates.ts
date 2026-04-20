0bd364510cb0d105885be181cfedf695e21f08ab
Ngo-seakyarith <seakyarith@gmail.com>
2026-04-20 18:26:08 +0700

    fix: move Candidate type definition to improve code organization

--- src/lib/candidates.ts
+++ src/lib/candidates.ts
@@ -1,5 +1,7 @@
-import type { Candidate, Prisma } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
+
+export type Candidate = Prisma.CandidateGetPayload<Prisma.CandidateDefaultArgs>;
 
 export type ExperienceItem = {
   position?: string;
